b32d9a7220c00f00e7d971630e4245f5b246cfb8
trananh99 <54661281+trananh99@users.noreply.github.com>
2020-09-01 14:05:54 +0700

    QuanLyThuVien_ThucTapNhom

---
+++ BaiTapNhom_ThuVien/KetNoiDB/dbConnection.cs
@@ -0,0 +1,336 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace KetNoiDB
+{
+    /// <summary>
+    /// Các tham số hệ thống dùng để thống nhất trong toàn bộ dự án
+    /// </summary>
+    public class AppConfig
+	{
+        private static string con;
+        public static string connectionString()
+        {
+            con = @"Data Source=.\VANANH;Initial Catalog=QL_ThuVien;Integrated Security=True";
+            return con;
+        }
+        public static int setConnect(string server, string database, string username, string userpassword)
+        {
+            con = string.Format("Server={0};Database={1};User Id={2};Password={3};",server,database,username,userpassword);
+            return 0;
+            
+        }       
+	}
+
+    public class dbConnection
+    {
+        private SqlDataAdapter myAdapter;
+        private SqlConnection conn;
+
+        /// <constructor>
+        /// Initialise Connection
+        /// </constructor>
+        public dbConnection()
+        {
+            myAdapter = new SqlDataAdapter();
+            conn = new SqlConnection(AppConfig.connectionString());
+        }
+
+        /// <method>
+        /// Open Database Connection if Closed or Broken
+        /// </method>
+        private SqlConnection openConnection()
+        {
+            if (conn.State == ConnectionState.Closed || conn.State ==ConnectionState.Broken)
+            {
+                conn.Open();
+            }
+            return conn;
+        }
+
+        /// <method>
+        /// Select Query
+        /// </method>
+        public DataTable executeSelectQuery(String _query, SqlParameter[] sqlParameter)
+        {
+            SqlCommand myCommand = new SqlCommand();
+            DataTable dataTable = new DataTable();
+            dataTable = null;
+            DataSet ds = new DataSet();
+            try
+            {
+                myCommand.Connection = openConnection();
+                myCommand.CommandText = _query;
+                myCommand.Parameters.AddRange(sqlParameter);
+                myCommand.ExecuteNonQuery();                
+                myAdapter.SelectCommand = myCommand;
+                myAdapter.Fill(ds);
+                dataTable = ds.Tables[0];
+            }
+            catch (SqlException e)
+            {
+                Console.Write("Error - Connection.executeSelectQuery - Query: " + _query + " \nException: " + e.StackTrace.ToString());
+                return null;
+            }
+            finally
+            {
+
+            }
+            return dataTable;
+        }
+        /// <method>
+        /// Select Query
+        /// </method>
+        public DataTable executeSelectQueryNonParameter(String _query)
+        {
+            SqlCommand myCommand = new SqlCommand();
+            DataTable dataTable = new DataTable();
+            dataTable = null;
+            DataSet ds = new DataSet();
+            try
+            {
+                myCommand.Connection = openConnection();
+                myCommand.CommandText = _query;
+                myCommand.ExecuteNonQuery();
+                myAdapter.SelectCommand = myCommand;
+                myAdapter.Fill(ds);
+                dataTable = ds.Tables[0];
+            }
+            catch (SqlException e)
+            {
+                Console.Write("Error - Connection.executeSelectQuery - Query: " + _query + " \nException: " + e.StackTrace.ToString());
+                return null;
+            }
+            finally
+            {
+
+            }
+            return dataTable;
+        }
+        /// <method>
+        /// Select Query with Procedure
+        /// </method>
+        public DataTable executeSelectProcedureQuery(String _query, SqlParameter[] sqlParameter)
+        {
+            SqlCommand myCommand = new SqlCommand();
+            DataTable dataTable = new DataTable();
+            dataTable = null;
+            DataSet ds = new DataSet();
+            try
+            {
+                myCommand.Connection = openConnection();
+                myCommand.CommandText = _query;
+                myCommand.CommandType = CommandType.StoredProcedure;
+                myCommand.Parameters.AddRange(sqlParameter);
+                myCommand.ExecuteNonQuery();
+                myAdapter.SelectCommand = myCommand;
+                myAdapter.Fill(ds);
+                dataTable = ds.Tables[0];
+            }
+            catch (SqlException e)
+            {
+                Console.Write("Error - Connection.executeSelectQuery - Query: " + _query + " \nException: " + e.StackTrace.ToString());
+                return null;
+            }
+            finally
+            {
+
+            }
+            return dataTable;
+        }
+        /// <method>
+        /// Select Query
+        /// </method>
+        public DataTable executeSelectProcedureQueryNonParameter(String _query)
+        {
+            SqlCommand myCommand = new SqlCommand();
+            DataTable dataTable = new DataTable();
+            dataTable = null;
+            DataSet ds = new DataSet();
+            try
+            {
+                myCommand.Connection = openConnection();
+                myCommand.CommandText = _query;
+                myCommand.CommandType = CommandType.StoredProcedure;
+                myCommand.ExecuteNonQuery();
+                myAdapter.SelectCommand = myCommand;
+                myAdapter.Fill(ds);
+                dataTable = ds.Tables[0];
+            }
+            catch (SqlException e)
+            {
+                Console.Write("Error - Connection.executeSelectQuery - Query: " + _query + " \nException: " + e.StackTrace.ToString());
+                return null;
+            }
+            finally
+            {
+
+            }
+            return dataTable;
+        }
+        /// <method>
+        /// Insert Query
+        /// </method>
+        public bool executeInsertQuery(String _query, SqlParameter[] sqlParameter)
+        {
+            SqlCommand myCommand = new SqlCommand();
+            try
+            {
+                myCommand.Connection = openConnection();
+                myCommand.CommandText = _query;
+                myCommand.Parameters.AddRange(sqlParameter);
+                myAdapter.InsertCommand = myCommand;
+                myCommand.ExecuteNonQuery();
+            }
+            catch (SqlException e)
+            {
+                Console.Write("Error - Connection.executeInsertQuery - Query: " + _query + " \nException: \n" + e.StackTrace.ToString());
+                return false;
+            }
+            finally
+            {
+                myAdapter.Dispose();
+                myCommand.Dispose();
+            }
+            return true;
+        }
+
+        /// <method>
+        /// Insert Query
+        /// </method>
+        public bool executeInsertProcedureQuery(String _query, SqlParameter[] sqlParameter)
+        {
+            SqlCommand myCommand = new SqlCommand();
+            try
+            {
+                myCommand.Connection = openConnection();
+                myCommand.CommandText = _query;
+                myCommand.CommandType = CommandType.StoredProcedure;
+                myCommand.Parameters.AddRange(sqlParameter);
+                myAdapter.InsertCommand = myCommand;
+                myCommand.ExecuteNonQuery();
+            }
+            catch (SqlException e)
+            {
+                Console.Write("Error - Connection.executeInsertQuery - Query: " + _query + " \nException: \n" + e.StackTrace.ToString());
+                return false;
+            }
+            finally
+            {
+                myAdapter.Dispose();
+                myCommand.Dispose();
+            }
+            return true;
+        }
+        /// <method>
+        /// Update Query
+        /// </method>
+        public bool executeUpdateQuery(String _query, SqlParameter[] sqlParameter)
+        {
+            SqlCommand myCommand = new SqlCommand();
+            try
+            {
+                myCommand.Connection = openConnection();
+                myCommand.CommandText = _query;
+                myCommand.Parameters.AddRange(sqlParameter);
+                myAdapter.UpdateCommand = myCommand;
+                myCommand.ExecuteNonQuery();
+            }
+            catch (SqlException e)
+            {
+                Console.Write("Error - Connection.executeUpdateQuery - Query: " + _query + " \nException: " + e.StackTrace.ToString());
+                return false;
+            }
+            finally
+            {
+                myAdapter.Dispose();
+                myCommand.Dispose();
+            }
+            return true;
+        }
+        /// <method>
+        /// Update Query
+        /// </method>
+        public bool executeUpdateProcedureQuery(String _query, SqlParameter[] sqlParameter)
+        {
+            SqlCommand myCommand = new SqlCommand();
+            try
+            {
+                myCommand.Connection = openConnection();
+                myCommand.CommandText = _query;
+                myCommand.CommandType = CommandType.StoredProcedure;
+                myCommand.Parameters.AddRange(sqlParameter);
+                myAdapter.UpdateCommand = myCommand;
+                myCommand.ExecuteNonQuery();
+            }
+            catch (SqlException e)
+            {
+                Console.Write("Error - Connection.executeUpdateQuery - Query: " + _query + " \nException: " + e.StackTrace.ToString());
+                return false;
+            }
+            finally
+            {
+                myAdapter.Dispose();
+                myCommand.Dispose();
+            }
+            return true;
+        }
+             /// <method>
+        /// Delete Query
+        /// </method>
+        public bool executeDeleteQuery(String _query, SqlParameter[] sqlParameter)
+        {
+            SqlCommand myCommand = new SqlCommand();
+            try
+            {
+                myCommand.Connection = openConnection();
+                myCommand.CommandText = _query;
+                myCommand.Parameters.AddRange(sqlParameter);
+                myAdapter.DeleteCommand= myCommand;
+                myCommand.ExecuteNonQuery();
+            }
+            catch (SqlException e)
+            {
+                Console.Write("Error - Connection.executeUpdateQuery - Query: " + _query + " \nException: " + e.StackTrace.ToString());
+                return false;
+            }
+            finally
+            {
+                myAdapter.Dispose();
+                myCommand.Dispose();
+            }
+            return true;
+        }
+        /// <method>
+        /// Update Procedure Query 
+        /// </method>
+        public bool executeDeleteProcedureQuery(String _query, SqlParameter[] sqlParameter)
+        {
+            SqlCommand myCommand = new SqlCommand();
+            try
+            {
+                myCommand.Connection = openConnection();
+                myCommand.CommandText = _query;
+                myCommand.CommandType = CommandType.StoredProcedure;
+                myCommand.Parameters.AddRange(sqlParameter);
+                myAdapter.DeleteCommand= myCommand;
+                myCommand.ExecuteNonQuery();
+            }
+            catch (SqlException e)
+            {
+                Console.Write("Error - Connection.executeUpdateQuery - Query: " + _query + " \nException: " + e.StackTrace.ToString());
+                return false;
+            }
+            finally
+            {
+                myAdapter.Dispose();
+                myCommand.Dispose();
+            }
+            return true;
+        }
+    }
+}
