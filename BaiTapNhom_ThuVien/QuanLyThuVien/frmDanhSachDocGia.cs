9ebafa96559aaab1381d6fae1e094d23d9c80891
DungLeMTA <duydung.glhd99@gmail.com>
2020-09-03 07:58:44 +0700

    Dũng

---
+++ BaiTapNhom_ThuVien/QuanLyThuVien/frmDanhSachDocGia.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BangThuVien;
+using KetNoiDB;
+
+
+namespace QuanLyThuVien
+{
+    public partial class frmDanhSachDocGia : Form
+    {
+        BUS_BanDoc bd = new BUS_BanDoc();
+        int chon;
+        TimKiem tk = new TimKiem();
+      
+        public frmDanhSachDocGia()
+        {
+            InitializeComponent();
+        }
+
+        public void KhoaDK()
+        {
+            txtCMND.Enabled = txtDiaChi.Enabled = txtDienThoai.Enabled = txtEmail.Enabled = txtHoTen.Enabled = txtMaLop.Enabled = cbGT.Enabled = dtpNgaySinh.Enabled = false;
+            btnThem.Enabled = btnSua.Enabled = btnXoa.Enabled = true;
+            btnLuu.Enabled = false;
+        }
+
+    }
+}
